a14b884a600303497ba1e9e56eea5787f9d7ec90
DheerajSom <63675896+Dheerajsom@users.noreply.github.com>
2022-05-29 16:21:24 -0700

    Update Lexer.java

--- Lexer.java
+++ Lexer.java
@@ -6,113 +6,99 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 public class Lexer {
-    private String code;
-    private int index; 
-    private char currentChar;
+    private StringBuilder input = new StringBuilder();
+    private Token token;
+    private String lexema;
+    private boolean exausthed = false;
+    private String errorMessage = "";
+    private Set<Character> blankChars = new HashSet<Character>();
 
-    public Lexer(String code) {
-        this.code = code;
-        this.index = 0;
-        this.currentChar = this.code.charAt(this.index);
+    public Lexer(String filePath) {
+        try (Stream<String> st = Files.lines(Paths.get(filePath))) {
+            st.forEach(input::append);
+        } catch (IOException ex) {
+            exausthed = true;
+            errorMessage = "Could not read file: " + filePath;
+            return;
+        }
+
+        blankChars.add('\r');
+        blankChars.add('\n');
+        blankChars.add((char) 8);
+        blankChars.add((char) 9);
+        blankChars.add((char) 11);
+        blankChars.add((char) 12);
+        blankChars.add((char) 32);
+
+        moveAhead();
+    }
+    public void moveAhead() {
+        if (exausthed) {
+            return;
+        }
+
+        if (input.length() == 0) {
+            exausthed = true;
+            return;
+        }
+
+        ignoreWhiteSpaces();
+
+        if (findNextToken()) {
+            return;
+        }
+
+        exausthed = true;
+
+        if (input.length() > 0) {
+            errorMessage = "Unexpected symbol: '" + input.charAt(0) + "'";
+        }
     }
 
-    public Token advance(){
-        this.index++;
-        if (this.index >= this.code.length()) {
-            this.currentChar = '\0';
-        } else {
-            this.currentChar = this.code.charAt(this.index);
+    private void ignoreWhiteSpaces() {
+        int charsToDelete = 0;
+
+        while (blankChars.contains(input.charAt(charsToDelete))) {
+            charsToDelete++;
         }
-        return this.nextToken();
+
+        if (charsToDelete > 0) {
+            input.delete(0, charsToDelete);
+        }
     }
 
+    private boolean findNextToken() {
+        for (Token t : Token.values()) {
+            int end = t.endOfMatch(input.toString());
 
-    public Token nextToken() {
-        while (Character.isWhitespace(this.currentChar)) {
-            this.advance();
+            if (end != -1) {
+                token = t;
+                lexema = input.substring(0, end);
+                input.delete(0, end);
+                return true;
+            }
         }
 
-        if (Character.isLetter(this.currentChar)) {
-            StringBuilder textCode = new StringBuilder();
-            while (Character.isLetter(this.currentChar)) {
-                textCode.append(this.currentChar);
-                this.advance();
-            }
-            String text = textCode.toString();
-            if (text.equals("based")) {
-                return Token.PRINT;
-            } else if (text.equals("ask")) {
-                return Token.INPUT;
-            } else if (text.equals("if")) {
-                return Token.IF;
-            } else if (text.equals("else")) {
-                return Token.ELSE;
-            } else if (text.equals("{")) {
-                return Token.OPENBRACKET;
-            } else if (text.equals("}")) {
-                return Token.CLOSEBRACKET;
-            } else if (text.equals(";")) {
-                return Token.SEMI;
-            } else if (text.equals(",")) {
-                return Token.COMMA;
-            } else if (text.equals("+")) {
-                return Token.PLUS;
-            } else if (text.equals("-")) {
-                return Token.MINUS;
-            } else if (text.equals("*")) {
-                return Token.MUL;
-            } else if (text.equals("/")) {
-                return Token.DIV;
-            } else if (text.equals("!")) {
-                return Token.NOT;
-            } else if (text.equals("&")) {
-                return Token.AND;
-            } else if (text.equals("|")) {
-                return Token.OR;
-            } else if (text.equals("<")) {
-                return Token.LESS;
-            } else if (text.equals("<=")) {
-                return Token.LEG;
-            } else if (text.equals(">")) {
-                return Token.GT;
-            }
-            else if(text.equals("<=")){
-                return Token.LEG;
-            }
-            else if(text.equals(">=")){
-                return Token.GEQ;
-            }
-            else if(text.equals("==")){
-                return Token.EQ;
-            }
-            else if(text.equals("=")){
-                return Token.ASSIGNOP;
-            }
-            else if(text.equals("(")){
-                return Token.PARENOPEN;
-            }
-            else if(text.equals(")")){
-                return Token.PARENCLOSE;
-            }
-            else if(text.equals("@")){
-                return Token.COMMENTS;
-            }
-            else if(text.equals("text")){
-                return Token.STRING;
-            }
-            else if(text.equals("num")){
-                return Token.INTEGER;
-            }
-            else if(text.equals("bool")){
-                return Token.BOOL;
-            }
-            else if(text.equals("ask")){
-                return Token.INPUT;
-            }
-            else{
-                return null;
-            }
-        }
-        return null;
+        return false;
+    }
+
+    public Token currentToken() {
+        return token;
+    }
+
+    public String currentLexema() {
+        return lexema;
+    }
+
+    public boolean isSuccessful() {
+        return errorMessage.isEmpty();
+    }
+
+    public String errorMessage() {
+        return errorMessage;
+    }
+
+    public boolean isExausthed() {
+        return exausthed;
     }
 }
