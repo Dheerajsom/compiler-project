067050eccfa43928e1d8250200074f3e4ead6dbc
DheerajSom <63675896+Dheerajsom@users.noreply.github.com>
2022-05-29 16:21:36 -0700

    Update based.java

--- based.java
+++ based.java
@@ -1,10 +1,23 @@
 import java.io.FilterInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class based {
 
     public static void main(String[] args) {
 
+        Lexer lexer = new Lexer("read.txt");
+        while (!lexer.isExausthed()) {
+            System.out.printf("%-5s :  %s \n",lexer.currentLexema() , lexer.currentToken());
+            lexer.moveAhead();
+        }
 
-
+        if (lexer.isSuccessful()) {
+            System.out.println("Done!");
+        } else {
+            System.out.println(lexer.errorMessage());
+        }
     }
 }
